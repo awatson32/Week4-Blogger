355cb35b290adbb68985457c9b7e03ee4dceb3c7
awatson32 <audrey.watson32@gmail.com>
2016-03-19 15:13:19 -0500

    more changes

--- client/js/controllers.js
+++ client/js/controllers.js
@@ -2,15 +2,12 @@
 
 var controllers = angular.module('myBlogApp.controllers', []);
 controllers.controller('BlogController', ['$scope', function($scope, BlogService) {
-    $scope.posts = BlogService.query();
+    $scope.getPosts = function() {
+        BlogService.query($scope.posts);
+    }
 }]);
 controllers.controller('NewPostController', ['$scope', function($scope, BlogService) {
     $scope.createPost = function() {
-        BlogService.save(
-            {
-                title: post.title,
-                author: post.author,
-                content: post.content
-            });
+        BlogService.save($scope.posts);
     }
 }]);
